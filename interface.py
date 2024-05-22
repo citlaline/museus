c8a4f8a3d7906b3d2f25345e6add4ddc3591da2a
citlaline <168496446+citlaline@users.noreply.github.com>
2024-05-22 15:40:06 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,7 +4,7 @@ st.title('Title')
 
 st.image("https://assets.masp.org.br/uploads/temp/WEB_IM_MASP_10834_01.jpg")
    if st.button('Click me'):
-   st.write('Button clicked!')
+      st.write('Button clicked!')
 
 with st.sidebar:
    st.header('Salut')
