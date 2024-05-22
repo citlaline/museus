d60953505454a74a4f7703a2124f2c4f83c19e34
citlaline <168496446+citlaline@users.noreply.github.com>
2024-05-22 15:39:34 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.title('Title')
 
+st.image("https://assets.masp.org.br/uploads/temp/WEB_IM_MASP_10834_01.jpg")
+   if st.button('Click me'):
+   st.write('Button clicked!')
+
 with st.sidebar:
    st.header('Salut')
    st.write('Seu aplicativo de dicas de saúde!')
