340bb9027f5f51fc93fe5e9926dadc605fde2914
citlaline <168496446+citlaline@users.noreply.github.com>
2024-05-22 15:41:37 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Title')
 
-st.image("https://assets.masp.org.br/uploads/temp/WEB_IM_MASP_10834_01.jpg", width=200)
+st.image("https://assets.masp.org.br/uploads/temp/WEB_IM_MASP_10834_01.jpg", width=50)
 if st.button('Click me'):
    st.write('Button clicked!')
 
