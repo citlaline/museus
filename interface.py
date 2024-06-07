a89585d052cc1aea79699c257b32934c33175ddf
citlaline <168496446+citlaline@users.noreply.github.com>
2024-06-07 12:37:56 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -3,9 +3,32 @@ import streamlit as st
 
 st.title('Title')
 
-st.image("https://assets.masp.org.br/uploads/temp/WEB_IM_MASP_10834_01.jpg", width=100)
-if st.button('Click me'):
-   st.write('Button clicked!')
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+   st.image("https://assets.masp.org.br/uploads/temp/WEB_AL_MASP_00267_01.jpg", width=100)
+   if st.button('Clique aqui'):
+      st.write('Imagem selecionada')
+      
+with col2:
+   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_C_01193_01.jpg", width=100)
+   if st.button('Clique aqui'):
+      st.write('Imagem selecionada')
+
+with col3:
+   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10811_01.jpg", width=100)
+   if st.button('Clique aqui'):
+      st.write('Imagem selecionada')
+
+with col4:
+   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10808_01.jpg", width=100)
+   if st.button('Clique aqui'):
+      st.write('Imagem selecionada')
+
+with col5:
+   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10802_01.jpg", width=100)
+   if st.button('Clique aqui'):
+      st.write('Imagem selecionada')
 
 with st.sidebar:
    st.header('Salut')
