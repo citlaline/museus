ffddfb4f40667d8190b9a77d37a42a77f9b5ae6e
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-06-09 09:38:24 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,31 +5,31 @@ st.title('Title')
 col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_AL_MASP_00267_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write('Imagem selecionada')
+   st.image_0("https://assets.masp.org.br/uploads/temp/WEB_AL_MASP_00267_01.jpg", width=100)
+   if st.button_0('Clique aqui'):
+      st.write_0('Imagem selecionada')
       
 with col2:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_C_01193_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write('Imagem selecionada')
+   st.image_1("https://assets.masp.org.br/uploads/temp/WEB_JB_C_01193_01.jpg", width=100)
+   if st.button_1('Clique aqui'):
+      st.write_1('Imagem selecionada')
 
 with col3:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10811_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write('Imagem selecionada')
+   st.image_2("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10811_01.jpg", width=100)
+   if st.button_2('Clique aqui'):
+      st.write_2('Imagem selecionada')
 
 with col4:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10808_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write('Imagem selecionada')
+   st.image_3("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10808_01.jpg", width=100)
+   if st.button_3('Clique aqui'):
+      st.write_3('Imagem selecionada')
 
 with col5:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10802_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write('Imagem selecionada')
+   st.image_4("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10802_01.jpg", width=100)
+   if st.button_4('Clique aqui'):
+      st.write_4('Imagem selecionada')
 
 with st.sidebar:
    st.header('Salut')
-   st.write('Seu aplicativo de dicas de saúde!')
+   st.write_5('Seu aplicativo de dicas de saúde!')
    st.caption('Criado por ...')
