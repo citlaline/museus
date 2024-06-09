e99f2de96e3877488198b779d410e37849a156c9
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-06-09 10:11:10 -0300

    Update teste_interface.py

--- teste_interface.py
+++ teste_interface.py
@@ -5,31 +5,31 @@ st.title('Title')
 col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_AL_MASP_00267_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write('Imagem selecionada')
-      
+    st.image("https://assets.masp.org.br/uploads/temp/WEB_AL_MASP_00267_01.jpg", width=100)
+    if st.button('Clique aqui', key='button1'):
+        st.write('Imagem selecionada')
+
 with col2:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_C_01193_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write_1('Imagem selecionada')
+    st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_C_01193_01.jpg", width=100)
+    if st.button('Clique aqui', key='button2'):
+        st.write('Imagem selecionada')
 
 with col3:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10811_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write_2('Imagem selecionada')
+    st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10811_01.jpg", width=100)
+    if st.button('Clique aqui', key='button3'):
+        st.write('Imagem selecionada')
 
 with col4:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10808_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write_3('Imagem selecionada')
+    st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10808_01.jpg", width=100)
+    if st.button('Clique aqui', key='button4'):
+        st.write('Imagem selecionada')
 
 with col5:
-   st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10802_01.jpg", width=100)
-   if st.button('Clique aqui'):
-      st.write_4('Imagem selecionada')
+    st.image("https://assets.masp.org.br/uploads/temp/WEB_JB_MASP_10802_01.jpg", width=100)
+    if st.button('Clique aqui', key='button5'):
+        st.write('Imagem selecionada')
 
 with st.sidebar:
-   st.header('Salut')
-   st.write_5('Seu aplicativo de dicas de saúde!')
-   st.caption('Criado por ...')
+    st.header('Salut')
+    st.write('Seu aplicativo de dicas de saúde!')
+    st.caption('Criado por ...')
